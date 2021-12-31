38756808a4536ce0c6baab3731e3ae90fed4a6b7
ThantSin17 <skybluestone7@gmail.com>
2021-12-31 22:34:56 +0630

    create surface view

--- app/src/main/java/com/stone/barcode/MainActivity.kt
+++ app/src/main/java/com/stone/barcode/MainActivity.kt
@@ -2,10 +2,31 @@ package com.stone.barcode
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.SurfaceHolder
+import androidx.databinding.DataBindingUtil
+import com.stone.barcode.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+
+        binding.surfaceView.holder.addCallback(object:SurfaceHolder.Callback{
+            override fun surfaceCreated(p0: SurfaceHolder) {
+                TODO("Not yet implemented")
+            }
+
+            override fun surfaceChanged(p0: SurfaceHolder, p1: Int, p2: Int, p3: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun surfaceDestroyed(p0: SurfaceHolder) {
+                TODO("Not yet implemented")
+            }
+
+        })
+
     }
 }
